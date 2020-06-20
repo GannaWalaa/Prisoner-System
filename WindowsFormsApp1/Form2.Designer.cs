c8afe6d1c01794bccef1bed29e2c375f38273cc3
Ganna Walaa <gannawalaa@gmail.com>
2020-06-20 16:55:23 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tele = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.duty = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.address = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.male = new System.Windows.Forms.RadioButton();
+            this.female = new System.Windows.Forms.RadioButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.prison = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter the data:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Officer ID:";
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(299, 119);
+            this.ID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(331, 22);
+            this.ID.TabIndex = 2;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(299, 149);
+            this.name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(331, 22);
+            this.name.TabIndex = 4;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Officer Name:";
+            // 
+            // tele
+            // 
+            this.tele.Location = new System.Drawing.Point(299, 238);
+            this.tele.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tele.Name = "tele";
+            this.tele.Size = new System.Drawing.Size(331, 22);
+            this.tele.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(45, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Officer Telephone Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(45, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Officer Gender:";
+            // 
+            // duty
+            // 
+            this.duty.Location = new System.Drawing.Point(299, 297);
+            this.duty.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.duty.Name = "duty";
+            this.duty.Size = new System.Drawing.Size(331, 22);
+            this.duty.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(45, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Officer Duty Time:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(155, 406);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 57);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(389, 406);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 57);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(299, 267);
+            this.address.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(331, 22);
+            this.address.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(45, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Officer Adress:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(45, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 20);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Officer Password:";
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(299, 208);
+            this.pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pass.Name = "pass";
+            this.pass.PasswordChar = '*';
+            this.pass.Size = new System.Drawing.Size(331, 22);
+            this.pass.TabIndex = 20;
+            this.pass.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // male
+            // 
+            this.male.AutoSize = true;
+            this.male.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.male.Location = new System.Drawing.Point(299, 363);
+            this.male.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.male.Name = "male";
+            this.male.Size = new System.Drawing.Size(61, 22);
+            this.male.TabIndex = 21;
+            this.male.TabStop = true;
+            this.male.Text = "Male";
+            this.male.UseVisualStyleBackColor = true;
+            // 
+            // female
+            // 
+            this.female.AutoSize = true;
+            this.female.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.female.Location = new System.Drawing.Point(439, 363);
+            this.female.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.female.Name = "female";
+            this.female.Size = new System.Drawing.Size(78, 22);
+            this.female.TabIndex = 22;
+            this.female.TabStop = true;
+            this.female.Text = "Female";
+            this.female.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(44, 182);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 20);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Officer Username:";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(299, 178);
+            this.username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(331, 22);
+            this.username.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(45, 330);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(153, 20);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Prison he works at:";
+            // 
+            // prison
+            // 
+            this.prison.Location = new System.Drawing.Point(299, 326);
+            this.prison.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.prison.Name = "prison";
+            this.prison.Size = new System.Drawing.Size(331, 22);
+            this.prison.TabIndex = 26;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(717, 504);
+            this.Controls.Add(this.prison);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.female);
+            this.Controls.Add(this.male);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.duty);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tele);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form2";
+            this.Text = "Updating Officer Information";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tele;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox duty;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.RadioButton female;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox prison;
+    }
+}
